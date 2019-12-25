5dc621ba75a62bbd474751a0ff04c1c5eb552997
jiaqi-xiao <594521354@qq.com>
2019-12-24 16:39:48 -0800

    add MD5 and Interceptor

---
+++ src/main/java/com/xjq/blog/interceptor/WebConfig.java
@@ -0,0 +1,18 @@
+package com.xjq.blog.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer { //WebMvcConfigurerAdapter 在Spring5.0已被废弃
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login");
+    }
+}
--- src/main/java/com/xjq/blog/service/UserServiceImpl.java
+++ src/main/java/com/xjq/blog/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.xjq.blog.service;
 
 import com.xjq.blog.dao.UserRepository;
 import com.xjq.blog.po.User;
+import com.xjq.blog.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
